53a0b9cffe57d587f9790685ace7f7af77eee542
Sabera Banu <saberabanu0001@gmail.com>
2025-12-25 13:25:17 +0900

    stramlit str ucture

--- app.py
+++ app.py
@@ -4,3 +4,14 @@ from inference import FaceAnalysis
 from PIL import Image
 
 st.set_page_config(page_title="Face Recognition", layout="centered")
+
+st.title("Face recognition system ):")
+st.write("Upload two face images to check whether they are same or not")
+
+@st.cache_resource
+def load_image():
+    return FaceAnalysis()
+
+
+
+
